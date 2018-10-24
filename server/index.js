cae5e0cddfc4085087f1d1ccd7d3a8aa3f67cee8
midnightmarth <midnightmarth@gmail.com>
2018-10-24 09:06:28 -0500

    updated to webpack 4. Also added listed endpoints in waffle.io

--- server/index.js
+++ server/index.js
@@ -1,22 +1,34 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path=require('path');
+const app = express();
 
-var app = express();
-
-app.use(express.static(__dirname + '/../react-client/dist'));
-
-
-app.get('/api', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.post('/api/UserId', (req, res) => {
+  console.log(req.body);
+  res.end();
 });
 
-app.listen(3000, function() {
+app.get('/api/:UserId', (req, res) => {
+  console.log(req.body);
+  res.end();
+});
+
+app.delete('/api/:UserId', (req, res) => {
+  console.log(req.body);
+  res.end();
+});
+app.post('/api/properties', (req, res) => {
+  console.log(req.body);
+  res.end();
+});
+console.log(path.resolve(__dirname,'../react-client/dist'));
+app.use(express.static(path.resolve(__dirname,'../react-client/dist')));
+
+
+// parse application/json
+
+
+app.listen(3000, () => {
   console.log('listening on port 3000!');
 });
-
