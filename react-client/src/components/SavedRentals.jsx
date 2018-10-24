25a7582c7d2efea15f3978e9a24f7b107516e7e1
Guillermo Maycotte <gmaycotte93@gmail.com>
2018-10-24 15:12:46 -0500

    SavedRentalItem comp done, tweek a little bit on SavedRentals.jsx so it matched with our list compwq

--- react-client/src/components/SavedRentals.jsx
+++ react-client/src/components/SavedRentals.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import ListItem from './ListItem';
+import React from 'react';
+import SavedRentalItem from './SavedRentalItem';
 
 
 class SavedRentals extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      savedRentals: []
+      savedRentals: [
+        {
+        "category": "austin.craigslist.org",
+        "date": "2018-10-24 11:04",
+        "hasPic": true,
+        "location": "(South Central)",
+        "pid": "6716480754",
+        "price": "$1070",
+        "title": "Massive Closets ~ Hardwoods ~ Close To St. Ed's University!",
+        "url": "https://austin.craigslist.org/apa/d/massive-closets-hardwoods/6716480754.html"
+    },
+    {
+        "category": "austin.craigslist.org",
+        "date": "2018-10-24 11:04",
+        "hasPic": true,
+        "location": "(3011 Whitis Ave)",
+        "pid": "6726748723",
+        "price": "$1150",
+        "title": "Parks and Recreation, Walkabilty 100%",
+        "url": "https://austin.craigslist.org/apa/d/parks-and-recreation/6726748723.html"
+    }
+  ]
     };
     this.updateSavedRentals = this.updateSavedRentals.bind(this);
   }
@@ -29,7 +50,7 @@ class SavedRentals extends React.Component {
         <ul>
           {this.state.savedRentals.map((item) => {
             <li>
-              <RentalsItem item={item}/>
+              <SavedRentalItem rental={item}/>
             </li>
             }
           )}
