826cf4ff760b6a7f6978f7479e911fd2c7a77d61
Guillermo Maycotte <gmaycotte93@gmail.com>
2018-10-24 10:17:32 -0500

    Search component ready to go, missing the function that should be passed down, replace console.log

--- react-client/src/components/Search.jsx
+++ react-client/src/components/Search.jsx
@@ -2,51 +2,54 @@ import React from "react";
 
 
 class Search extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            location: ''
-        };
-        this.onChange = this.onChange.bind(this);
-        this.onKeyPress = this.onKeyPress.bind(this);
-        this.reset = this.reset.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: ''
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onKeyPress = this.onKeyPress.bind(this);
+    this.reset = this.reset.bind(this);
+  }
 
-    changeState(loc) {
-        this.setState({
-            location: loc
-        });
+  // set state
+  changeState(loc) {
+    this.setState({
+      location: loc
+    });
 
-    }
+  }
 
-    onChange(e) {
-        this.changeState(e.target.value);
-    }
+  // set state while typing
+  onChange(e) {
+    this.changeState(e.target.value);
+  }
 
-    onKeyPress(e) {
-        if(e.which === 13) {
-            this.changeState(e.target.value);
-            console.log(this.state.location);
-            this.reset();
-        }
+  // set state on "return" key
+  onKeyPress(e) {
+    if (e.which === 13) {
+      this.changeState(e.target.value);
+      console.log(this.state.location);
+      this.reset();
     }
+  }
 
-    reset() {
-        this.setState({
-            location: ''
-        });
-    }
+  // reset state
+  reset() {
+    this.setState({
+      location: ''
+    });
+  }
 
-    render() {
-        return (
-            <div>
-            <h4>Search</h4>
-            Enter a location: <input value={this.state.location} onChange={this.onChange} onKeyPress={this.onKeyPress}/>       
-            <button onClick={() => { console.log(this.state.location); this.reset()}}> search </button>
-           
-          </div>
-        )
-    }
+  render() {
+    return ( 
+      <div>
+        <h4> Search </h4>
+        Enter a location: <input value = {this.state.location} onChange = {this.onChange} onKeyPress = {this.onKeyPress}/>        
+        <button onClick = {() => {console.log(this.state.location); this.reset()}}> search </button>
+      </div>
+    )
+  }
 }
 
 
