a8b51714fa5adb7995c68d42f7ef69054840e7ea
Guillermo Maycotte <gmaycotte93@gmail.com>
2018-10-24 11:41:15 -0500

    Modified names to mach master

--- react-client/src/components/SavedRentals.jsx
+++ react-client/src/components/SavedRentals.jsx
@@ -2,34 +2,34 @@ import React from 'react'
 import ListItem from './ListItem';
 
 
-class SavedList extends React.Component {
+class SavedRentals extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      savedList: []
+      savedRentals: []
     };
-    this.updateSavedList = this.updateSavedList.bind(this);
+    this.updateSavedRentals = this.updateSavedRentals.bind(this);
   }
 
-  updateSavedList () {
+  updateSavedRentals () {
     let savedArr = [];
     this.props.saved.map((item) => {
         savedArr.push(item);
       }
     );
     this.setState({
-      savedList: savedArr
+      savedRentals: savedArr
     });
   }
 
   render() {
     return (
       <div>
-        <h1>Saved list</h1>
+        <h1>Saved Rentals</h1>
         <ul>
-          {this.state.savedList.map((item) => {
+          {this.state.savedRentals.map((item) => {
             <li>
-              <ListItem item={item}/>
+              <RentalsItem item={item}/>
             </li>
             }
           )}
@@ -39,4 +39,4 @@ class SavedList extends React.Component {
   }
 }
 
-export default SavedList
+export default SavedRentals
