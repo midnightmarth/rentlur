28a3a8ffc0724fb58ca7b70787555e2db3be2270
Nikolas Mentakis <nmentakis@Nikolass-MacBook-Pro.local>
2018-10-24 11:11:00 -0500

    quick fixes for gitignore and some react components

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,41 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import List from './components/List.jsx';
 import axios from 'axios';
+import Search from './components/Search.jsx'
+import List from './components/List.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      rentals: []
-    }
+    this.state = {
+      rentals: [],
+    };
     this.searchProperties = this.searchProperties.bind(this);
   }
-
+  // to be completed later
   componentDidMount() {
 
   }
-
+  //requires routes
   searchProperties(searchQuery) {
     const post = {
       method: 'post',
       url: '/api/properties',
       body: {
-        q: searchQuery
-      }
-    }
-
-    axios(post).then(response => {
+        q: searchQuery,
+      },
+    };
+  
+    axios(post).then((response) => {
       console.log(response);
-    })
+    });
   }
 
-  render () {
-    return (<div>
-      <h1>Rentlur</h1>
-      <Search search={this.searchProperties}/>
-    </div>)
+  render() {
+    return (
+      <div>
+        <h1>Rentlur</h1>
+        <Search search={this.searchProperties} />
+      </div>
+    );
   }
 }
 
