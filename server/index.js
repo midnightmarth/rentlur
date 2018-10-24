53b8e85a87047c5f1fe2c622d8880e11b5563f48
midnightmarth <midnightmarth@gmail.com>
2018-10-24 10:27:24 -0500

    added : to index.js server file

--- server/index.js
+++ server/index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path=require('path');
+const path = require('path');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.post('/api/UserId', (req, res) => {
+
+//authentication
+app.post('/api/login', (req, res) =>{
+  console.log('requested to login');
+  res.end
+})
+app.get('/api/logout', (req, res) =>{
+  console.log('requested to logout');
+  res.end
+})
+app.post('/api/signup', (req, res) =>{
+  console.log('requested to signup');
+  res.end
+})
+//
+
+app.post('/api/:UserId', (req, res) => {
   console.log(req.body);
   res.end();
 });
@@ -22,12 +39,10 @@ app.post('/api/properties', (req, res) => {
   console.log(req.body);
   res.end();
 });
-console.log(path.resolve(__dirname,'../react-client/dist'));
-app.use(express.static(path.resolve(__dirname,'../react-client/dist')));
-
+console.log(path.resolve(__dirname, '../react-client/dist'));
+app.use(express.static(path.resolve(__dirname, '../react-client/dist')));
 
 // parse application/json
-
 
 app.listen(3000, () => {
   console.log('listening on port 3000!');
