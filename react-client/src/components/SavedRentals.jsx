ac648cf0a796c429a5a3acafb315aa6c2b1e61cc
Guillermo Maycotte <gmaycotte93@gmail.com>
2018-10-24 17:22:17 -0500

    Made css file, included in html, refactored SavedRentals.jsx for functional rendering (much simpler), made nav bar for conditional rendering in index.jsx, somehow git made me commit List.jsx, did not changed anything

--- react-client/src/components/SavedRentals.jsx
+++ react-client/src/components/SavedRentals.jsx
@@ -1,63 +1,25 @@
 import React from 'react';
-import SavedRentalItem from './SavedRentalItem';
+import SavedRentalItem from './SavedRentalItem.jsx';
 
 
-class SavedRentals extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      savedRentals: [
-        {
-        "category": "austin.craigslist.org",
-        "date": "2018-10-24 11:04",
-        "hasPic": true,
-        "location": "(South Central)",
-        "pid": "6716480754",
-        "price": "$1070",
-        "title": "Massive Closets ~ Hardwoods ~ Close To St. Ed's University!",
-        "url": "https://austin.craigslist.org/apa/d/massive-closets-hardwoods/6716480754.html"
-    },
-    {
-        "category": "austin.craigslist.org",
-        "date": "2018-10-24 11:04",
-        "hasPic": true,
-        "location": "(3011 Whitis Ave)",
-        "pid": "6726748723",
-        "price": "$1150",
-        "title": "Parks and Recreation, Walkabilty 100%",
-        "url": "https://austin.craigslist.org/apa/d/parks-and-recreation/6726748723.html"
-    }
-  ]
-    };
-    this.updateSavedRentals = this.updateSavedRentals.bind(this);
-  }
-
-  updateSavedRentals () {
-    let savedArr = [];
-    this.props.saved.map((item) => {
-        savedArr.push(item);
-      }
-    );
-    this.setState({
-      savedRentals: savedArr
-    });
-  }
-
-  render() {
-    return (
+const SavedRentals = (props) => (
       <div>
         <h1>Saved Rentals</h1>
         <ul>
-          {this.state.savedRentals.map((item) => {
+          {props.saved.map( (item) => (
             <li>
               <SavedRentalItem rental={item}/>
             </li>
-            }
-          )}
+            ))
+          }
+          
         </ul>
       </div>
-    )
-  }
-}
+
+)
+  
+    
+  
+
 
 export default SavedRentals
