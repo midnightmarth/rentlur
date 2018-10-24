c06393abc7fa455e764061d37a58efb7ad795dc3
johnpwarren@gmail.com <johnpwarren@gmail.com>
2018-10-24 09:32:26 -0700

    Added route for searching properties" ;

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const craigslist = require('node-craigslist');
 
 var app = express();
 
 app.use(express.static(__dirname + '/../react-client/dist'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+let client = new craigslist.Client({
+  baseHost: 'craigslist.com',
+  city: 'Austin',
+})
 
 
 app.get('/api', function (req, res) {
@@ -16,7 +27,34 @@ app.get('/api', function (req, res) {
   });
 });
 
-app.listen(3000, function() {
+app.post('/api/search', (req, res) => {
+  const baseHost = req.body.baseHost || 'craigslist.org';
+  const category = req.body.category || 'hhh';
+  const maxAsk = req.body.maxAsk || '50000';
+  const minAsk = req.body.minAsk || '0';
+  const city = req.body.city || 'Austin';
+  const postal = req.body.postal || '78701';
+  const searchDistance = req.body.searchDistance || '25';
+
+  const searchQuery = {
+    baseHost,
+    category,
+    city,
+    maxAsk,
+    minAsk,
+    postal,
+    searchDistance,
+  };
+
+  client.search(searchQuery, '', (err, data) => {
+    console.log(data);
+    res.status(201).end(JSON.stringify(data));
+  
+  })
+})
+
+
+app.listen(1337, function() {
   console.log('listening on port 3000!');
 });
 
