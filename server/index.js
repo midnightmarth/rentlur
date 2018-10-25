b48461ac1c907b25d378e54e5052339773dd9fbd
midnightmarth <midnightmarth@gmail.com>
2018-10-25 11:15:31 -0500

    In the middle of auth

--- server/index.js
+++ server/index.js
@@ -1,24 +1,60 @@
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require('path');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const LocalStrategy = require('passport-local').Strategy;
+const cookieParser = require('cookie-parser');
+const keys = require('../credentials').web;
+
+// Google Authentication "Stradegy"
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.client_id,
+      clientSecret: keys.client_secret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+      console.log(refreshToken);
+      console.log(profile);
+      console.log(done);
+      return done(null, profile);
+    },
+  ),
+);
+//
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  secret: 'MemesAreCool',
+  saveUninitialized: false,
+  resave: false,
+}));
 
-//authentication
-app.post('/api/login', (req, res) =>{
-  console.log('requested to login');
-  res.end
-})
-app.get('/api/logout', (req, res) =>{
+// authentication
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  }),
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+app.get('/api/logout', (req, res) => {
   console.log('requested to logout');
-  res.end
-})
-app.post('/api/signup', (req, res) =>{
+  res.end();
+});
+app.post('/api/signup', (req, res) => {
   console.log('requested to signup');
-  res.end
-})
+  res.end();
+});
 //
 
 app.post('/api/:UserId', (req, res) => {
---
+++ server/passport/passport.js
@@ -0,0 +1,5 @@
+let googleStrategy = require('passport-google-oauth20');
+
+module.exports = (passport) => {
+
+}
