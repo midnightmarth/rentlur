fb8a4d99d2aa8d7193e17b84b6a909d94a0bed49
Nikolas Mentakis <nmentakis@gmail.com>
2018-10-26 18:59:56 -0500

    Parsing server/heroku

--- server/index.js
+++ server/index.js
@@ -21,25 +21,6 @@ app.use('/api/search', search);
 app.use('/api/properties', db);
 app.use('/api', authRoutes);
 
-app.post('/api/:UserId', (req, res) => {
-  console.log(req.body);
-  res.end();
-});
-
-app.get('/api/:UserId', (req, res) => {
-  console.log(req.body);
-  res.end();
-});
-
-app.delete('/api/:UserId', (req, res) => {
-  console.log(req.body);
-  res.end();
-});
-app.post('/api/properties', (req, res) => {
-  console.log(req.body);
-  res.end();
-});
-
 app.use(express.static(path.resolve(__dirname, '../react-client/dist')));
 
 app.get('/*', function(req, res) {
@@ -50,8 +31,6 @@ app.get('/*', function(req, res) {
   })
 })
 // parse application/json
-
-app.use(express.static(path.resolve(__dirname, '../react-client/dist')));
 
 // Setup
 
