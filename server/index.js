963d2d097fece0c2615a7e5d8fc37bf6ff5eedd8
midnightmarth <midnightmarth@gmail.com>
2018-10-26 10:43:16 -0500

    Added login capability, just need to know what frontend needs.

--- server/index.js
+++ server/index.js
@@ -1,85 +1,67 @@
-const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const path = require('path');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-// const LocalStrategy = require('passport-local').Strategy;
-const cookieParser = require('cookie-parser');
-const keys = require('../credentials').web;
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const keys = require("../credentials").web;
+const expressLogging = require('express-logging');
+const logger=require('logops');
+require('./auth');
+const passport = require('passport')
 
-// Google Authentication "Stradegy"
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.client_id,
-      clientSecret: keys.client_secret,
-      callbackURL: '/auth/google/callback',
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
-      console.log(refreshToken);
-      console.log(profile);
-      console.log(done);
-      return done(null, profile);
-    },
-  ),
-);
-//
-const craigslist = require('node-craigslist');
+const craigslist = require("node-craigslist");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(session({
-  secret: 'MemesAreCool',
-  saveUninitialized: false,
-  resave: false,
-}));
 
-// authentication
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
-  }),
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-
-// parse application/json
-app.use(bodyParser.json());
+app.use(expressLogging(logger));
+app.use(express.static(path.resolve(__dirname, "../react-client/dist")));
 
 const craigsList = new craigslist.Client({
-  baseHost: 'craigslist.com',
-  city: 'Austin',
+  baseHost: "craigslist.com",
+  city: "Austin"
 });
 
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "MemesAreCool",
+    resave: false,
+    saveUninitialized: true
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
 // authentication
-app.post('/api/login', (req, res) => {
-  console.log('requested to login');
+app.post("/api/login", passport.authenticate('local'), (req, res) => {
+  console.log('Authenticated');
+  //What do I send to let the client know it succeeded to login?
+  res.send({ data: 'Authenticated' });
+});
+
+
+app.get("/api/logout", (req, res) => {
+  console.log("requested to logout");
   res.end();
 });
-app.get('/api/logout', (req, res) => {
-  console.log('requested to logout');
-  res.end();
-});
-app.post('/api/signup', (req, res) => {
-  console.log('requested to signup');
+app.post("/api/signup", (req, res) => {
+  console.log("requested to signup");
   res.end();
 });
 //
 
-app.post('/api/search', (req, res) => {
-  console.log('made it into server')
-  const baseHost = req.body.baseHost || 'craigslist.org';
-  const category = req.body.category || 'hhh';
-  const maxAsk = req.body.maxAsk || '50000';
-  const minAsk = req.body.minAsk || '0';
-  const city = req.body.city || 'Austin';
-  const postal = req.body.postal || '78701';
-  const searchDistance = req.body.searchDistance || '25';
+app.post("/api/search", (req, res) => {
+  console.log("made it into server");
+  const baseHost = req.body.baseHost || "craigslist.org";
+  const category = req.body.category || "hhh";
+  const maxAsk = req.body.maxAsk || "50000";
+  const minAsk = req.body.minAsk || "0";
+  const city = req.body.city || "Austin";
+  const postal = req.body.postal || "78701";
+  const searchDistance = req.body.searchDistance || "25";
 
   const searchQuery = {
     baseHost,
@@ -88,42 +70,40 @@ app.post('/api/search', (req, res) => {
     maxAsk,
     minAsk,
     postal,
-    searchDistance,
+    searchDistance
   };
 
-  craigsList.search(searchQuery, '', (err, data) => {
+  craigsList.search(searchQuery, "", (err, data) => {
     if (err) {
       console.log(err);
       throw err;
     } else {
-      console.log('data in the search', data);
+      console.log("data in the search", data);
       res.json(data);
     }
   });
 });
 
-app.post('/api/:UserId', (req, res) => {
-  console.log(req.body);
+app.post("/api/:UserId", (req, res) => {
+  console.log('posting users',req.body);
   res.end();
 });
 
-app.get('/api/:UserId', (req, res) => {
-  console.log(req.body);
+app.get("/api/:UserId", (req, res) => {
+  console.log('getting usersss',req.params);
   res.end();
 });
 
-app.delete('/api/:UserId', (req, res) => {
-  console.log(req.body);
+app.delete("/api/:UserId", (req, res) => {
+  console.log('Deleteing something',req.body);
   res.end();
 });
-app.post('/api/properties', (req, res) => {
-  console.log(req.body);
+app.post("/api/properties", (req, res) => {
+  console.log('getting properties',req.body);
   res.end();
 });
-
-app.use(express.static(path.resolve(__dirname, '../react-client/dist')));
 
 // parse application/json
 app.listen(3000, () => {
-  console.log('listening on port 3000!');
+  console.log("listening on port 3000!");
 });
--- server/passport/passport.js
+++
@@ -1,5 +0,0 @@
-let googleStrategy = require('passport-google-oauth20');
-
-module.exports = (passport) => {
-
-}
